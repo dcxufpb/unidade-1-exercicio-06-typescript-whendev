9d26eccb7180b47cddf45c59e10bc9bcdc9200ed
whendev <whendel.morais.9@gmail.com>
2020-09-24 17:15:25 -0300

    exercise solved

--- cupom.test.ts
+++ cupom.test.ts
@@ -92,25 +92,25 @@ test('Loja Completa', () => {
 test('Nome vazio', () => {
   cupom.dados.nome_loja = "";
   verificaCampoObrigatorio(`O campo nome da loja é obrigatório`);
-  cupom.dados.nome_loja = "Arcos Dourados Com. de Alimentos LTDA";
+  cupom.dados.nome_loja = "Loja 1";
 });
 
 test('Logradouro vazio', () => {
   cupom.dados.logradouro = "";
   verificaCampoObrigatorio(`O campo logradouro do endereço é obrigatório`);
-  cupom.dados.logradouro = "Av. Projetada Leste";
+  cupom.dados.logradouro = "Log 1";
 });
 
 test('Número zero', () => {
   cupom.dados.numero = 0;
   expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_NUMERO);
-  cupom.dados.numero = 500;
+  cupom.dados.numero = 10;
 });
 
 test('Complemento vazio', () => {
   cupom.dados.complemento = "";
   expect(cupom.dados_loja()).toBe(TEXTO_ESPERADO_SEM_COMPLEMENTO);
-  cupom.dados.numero = "C1";
+  cupom.dados.complemento = "C1";
 });
 
 test('Bairro vazio', () => {
@@ -122,13 +122,13 @@ test('Bairro vazio', () => {
 test('Município vazio', () => {
   cupom.dados.municipio = "";
   verificaCampoObrigatorio(`O campo município do endereço é obrigatório`);
-  cupom.dados.municipio = "Campinas";
+  cupom.dados.municipio = "Mun 1";
 });
 
 test('Estado vazio', () => {
   cupom.dados.estado = "";
   verificaCampoObrigatorio(`O campo estado do endereço é obrigatório`);
-  cupom.dados.estado = "SP";
+  cupom.dados.estado = "E1";
 });
 
 test('CEP vazio', () => {
@@ -152,33 +152,39 @@ test('Observação vazia', () => {
 test('CNPJ vazio', () => {
   cupom.dados.cnpj = "";
   verificaCampoObrigatorio(`O campo CNPJ da loja é obrigatório`);
-  cupom.dados.cnpj = "42.591.651/0797-34";
+  cupom.dados.cnpj = "11.111.111/1111-11";
 });
 
 test('Inscrição estadual vazia', () => {
   cupom.dados.inscricao_estadual = "";
   verificaCampoObrigatorio(`O campo inscrição estadual da loja é obrigatório`);
-  cupom.dados.inscricao_estadual = "244.898.500.113";
+  cupom.dados.inscricao_estadual = "123456789";
 });
 
 test('Exercício 2 - customizado', () => {
 
-  // Defina seus próprios valores para as variáveis a seguir
-  cupom.dados.nome_loja = "";
-  cupom.dados.logradouro = "";
-  cupom.dados.numero = 0;
+//   // Defina seus próprios valores para as variáveis a seguir
+  cupom.dados.nome_loja = "LOJAS AMERICANAS S.A.";
+  cupom.dados.logradouro = "R SACADURA CABRAL";
+  cupom.dados.numero = 102;
   cupom.dados.complemento = "";
-  cupom.dados.bairro = "";
-  cupom.dados.municipio = "";
-  cupom.dados.estado = "";
-  cupom.dados.cep = "";
-  cupom.dados.telefone = "";
-  cupom.dados.observacao = "";
-  cupom.dados.cnpj = "";
-  cupom.dados.inscricao_estadual = "";
+  cupom.dados.bairro = "GAMBOA";
+  cupom.dados.municipio = "RIO DE JANEIRO";
+  cupom.dados.estado = "RJ";
+  cupom.dados.cep = "20.221-160";
+  cupom.dados.telefone = "(21) 2206-6708";
+  cupom.dados.observacao = "47.11-3-02 Comercio varejista de mercadorias em geral";
+  cupom.dados.cnpj = "33.014.556/0001-96";
+  cupom.dados.inscricao_estadual = "85.687.08-5";
 
   //E atualize o texto esperado abaixo
   expect(cupom.dados_loja()).toBe(
-    `
+    `LOJAS AMERICANAS S.A.
+R SACADURA CABRAL, 102
+GAMBOA - RIO DE JANEIRO - RJ
+CEP:20.221-160 Tel (21) 2206-6708
+47.11-3-02 Comercio varejista de mercadorias em geral
+CNPJ: 33.014.556/0001-96
+IE: 85.687.08-5
 `);
 });
